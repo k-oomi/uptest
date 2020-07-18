e9a0d87c0941b2b097f5b3982abdc181da9d3341
kaomi <minami4349@gmail.com>
2020-07-18 14:45:32 +0900

    csvモジュール読み込み

--- routes/common.js
+++ routes/common.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const fs = require('fs');
+const csv = require('csv');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('common');
+  res.render('common',{});
 });
 
 module.exports = router;
