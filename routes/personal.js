1c08a0f873d4a61df0354ddb2befa7e107fb0afc
kaomi <minami4349@gmail.com>
2020-07-19 01:35:49 +0900

    ログイン続きエラー

--- routes/personal.js
+++ routes/personal.js
@@ -3,14 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-//   if (req.session.data != undefined){
-//     if (req.session.data[0] > 0) {
-//       res.render('personal', {});
-//     }
-//   }else{
-//     res.redirect('/common');
-//   }
-res.render('personal', {});
+  if (req.session.data != undefined){
+    if (req.session.data[0] > 0) {
+      res.render('personal', {});
+    }
+  }else{
+    res.redirect('/common');
+  }
+// res.render('personal', {});
   
 });
 
