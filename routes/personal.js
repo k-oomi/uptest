31e0b707642c10ea5807dd285f99639b36375461
kaomi <minami4349@gmail.com>
2020-07-19 16:12:10 +0900

    sessionの値取り出し

--- routes/personal.js
+++ routes/personal.js
@@ -4,9 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   if (req.session.login != undefined){
-   
+      let opt = {
+        data: req.session.login
+      };
         
-      res.render('personal');
+      res.render('personal',opt);
     
   }else{
     res.redirect('/');
