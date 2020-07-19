531a7b42866ba9320eb250ac4805ab3a058fd22d
kaomi <minami4349@gmail.com>
2020-07-19 11:11:23 +0900

    とりあえず保存

--- routes/personal.js
+++ routes/personal.js
@@ -4,13 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   if (req.session.login != undefined){
-    if (req.session.login[0] > 0) {
-      res.render('personal', {});
-    }
+   
+        
+      res.render('personal');
+    
   }else{
     res.redirect('/');
   }
-res.render('personal', {});
+
+
   
 });
 
