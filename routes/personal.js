80813679593e1ae28732d6395bae8d1f21267c9d
kaomi <minami4349@gmail.com>
2020-07-23 17:07:21 +0900

    コミット

--- routes/personal.js
+++ routes/personal.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   if (req.session.login != undefined){
+    
+    let fileName = fs.readdirSync("public/csv");
+
       let opt = {
-        data: req.session.login
+        data: req.session.login,
+        file: fileName
       };
         
       res.render('personal',opt);
