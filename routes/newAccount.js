2f0b201962fcacc6606e42c99f822decbcd8f1fe
kaomi <minami4349@gmail.com>
2020-07-24 08:35:04 +0900

    アカウント作成機能追加

--- routes/newAccount.js
+++ routes/newAccount.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var dbdo = require('../db/exec.js');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
+    
+    
   
   res.render('newAccount');
+});
+
+
+//POST
+router.post('/', async function(req, res, next) {
+    let mail = req.body.email;
+    let pass = req.body.password;
+    let sql = "insert into users (mailadd,password) values('" + mail + "','" + pass + "')";
+    await dbdo.exec(sql);
+    res.render('common');
 });
 
 module.exports = router;
