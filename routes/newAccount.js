a5a7d03432e9d7213916a6f44664ea3a2ba60753
kaomi <minami4349@gmail.com>
2020-07-24 11:17:07 +0900

    アカウント登録機能追加

--- routes/newAccount.js
+++ routes/newAccount.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
+
 var dbdo = require('../db/exec.js');
+var dbget = require('../db/get.js');
+var dball = require('../db/all.js');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    let opt ={
+        message: null,
+        mail: null
+    };
     
     
   
-  res.render('newAccount');
+  res.render('newAccount',opt);
 });
 
 
@@ -16,9 +23,26 @@ router.get('/', function(req, res, next) {
 router.post('/', async function(req, res, next) {
     let mail = req.body.email;
     let pass = req.body.password;
-    let sql = "insert into users (mailadd,password) values('" + mail + "','" + pass + "')";
-    await dbdo.exec(sql);
-    res.render('common');
+    let sql = "select * from users where mailadd = '" + mail + "'";
+    let record = await dbget.getRow(sql);
+    if (record != undefined){
+        let opt ={
+            message:"重複エラー",
+            mail: record.mailadd
+        };
+        res.render('newAccount',opt);       
+    }else{
+        sql = "insert into users (mailadd,password) values('" + mail + "','" + pass + "')";
+        await dbdo.exec(sql);
+        let opt ={
+            message:"登録完了",
+            mail: mail
+        };
+        res.render('newAccount',opt);
+
+    }
+
+
 });
 
 module.exports = router;
